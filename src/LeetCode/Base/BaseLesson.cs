417fdde9a9af93291c792aa907f05382173ad2d0
奉伟 <landwind1180@gmail.com>
2019-12-25 14:39:22 +0800

    initial commit

---
+++ src/LeetCode/Base/BaseLesson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public  class BaseLesson
+    {
+        public virtual int Index { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Desc { get; set; }
+        public virtual string Url { get; set; }
+    }
+}
